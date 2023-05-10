dbcd331c42f5593fb42db19b3a539d600b3ae4a5
Prasenjit Purohit <prasenjit@prasenjit.net>
2023-05-10 04:20:53 -0700

    another try

--- build.gradle.kts
+++ build.gradle.kts
@@ -92,9 +92,5 @@ publishing {
 }
 
 signing {
-    val signingKey: String? by project
-    val signingPassword: String? by project
-//    val signingKeyId: String? by project
-    useInMemoryPgpKeys(signingKey, signingPassword)
     sign(publishing.publications["maven"])
 }
